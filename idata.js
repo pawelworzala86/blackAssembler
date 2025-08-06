d782c89d72f979d30e955ac6914f53a52d98d469
Pawel Worzala <pawelworzala86@gmail.com>
2025-08-06 21:05:06 +0200

    simple compile

---
+++ idata.js
@@ -0,0 +1,170 @@
+const fs = require('fs');
+
+let idata = `  dd 0,0,0,RVA kernel_name,RVA kernel_table
+  dd 0,0,0,RVA msvcrt_name,RVA msvcrt_table
+  dd 0,0,0,0,0
+
+  kernel_table:
+    ExitProcess:
+     dq RVA _ExitProcess
+    dq 0
+  msvcrt_table:
+    printf:
+     dq RVA _printf
+    dq 0
+
+  kernel_name:
+   db 'KERNEL32.DLL',0
+  msvcrt_name:
+   db 'MSVCRT.DLL',0
+
+  _ExitProcess:
+   dw 0
+    db 'ExitProcess',0
+  _printf:
+   dw 0
+    db 'printf',0`
+
+
+let OFFSET = 0x3000
+const CALLS = {}
+
+
+
+function LE(text){
+    let bytes = text.match(/.{2}/g);
+    return bytes.reverse().join(' ');
+}
+function DectoHex1(dec){
+    let val = parseInt(dec)
+    val = val.toString(16)
+    return val
+}
+function DectoHex2(dec){
+    let val = parseInt(dec)
+    val = val.toString(16)
+    val = val.padStart(4,'00')
+    return val
+}
+function DectoHex4(dec){
+    let val = parseInt(dec)
+    val = val.toString(16)
+    val = val.padStart(8,'00')
+    return val
+}
+function DectoHex8(dec){
+    let val = parseInt(dec)
+    val = val.toString(16)
+    val = val.padStart(16,'00')
+    return val
+}
+
+
+let lines = idata.split('\n')
+function parse(){
+lines = lines.map(line=>{
+    line=line.trim()
+    if(line.split(' ')[0]=='dd'){
+        let val = line.replace('dd','').trim().split(',').map(v=>v.trim()).map(v=>{
+            if(v.indexOf('RVA')>-1){
+                let vn = v.replace('RVA ','').trim()
+                console.log('vn',vn)
+                if(CALLS[vn]){
+                    return 'RVA '+CALLS[vn]
+                }else{
+                    return 'RVA '+vn
+                }
+            }
+            return v
+        })
+        OFFSET+=val.length*4
+        line = 'dd '+val.join(',')
+    }
+    if(line.split(' ')[0]=='dq'){
+        let val = line.replace('dq','').trim().split(',').map(v=>v.trim()).map(v=>{
+            if(v.indexOf('RVA')>-1){
+                let vn = v.replace('RVA ','').trim()
+                console.log('vn',vn)
+                if(CALLS[vn]){
+                    return 'RVA '+CALLS[vn]
+                }else{
+                    return 'RVA '+vn
+                }
+            }
+            return v
+        })
+        OFFSET+=val.length*8
+        line = 'dq '+val.join(',')
+    }
+    if(line.split(' ')[0]=='dw'){
+        let val = line.replace('dw','').trim().split(',').map(v=>v.trim())
+        OFFSET+=val.length*2
+    }
+    if(line.split(' ')[0]=='db'){
+        let val = line.replace('db','').trim().split('\'')[1]
+        OFFSET+=val.length
+    }
+    line=line.replace(/([a-zA-Z0-9\_]+)\:/gm,match=>{
+        let name = match.split(':')[0].trim()
+        CALLS[name] = OFFSET
+        return ''
+    })
+    return line
+})
+}
+parse()
+console.log('CALLS',CALLS)
+parse()
+
+
+
+lines = lines.map(line=>{
+    let type = line.split(' ')[0]
+    let val = line.replace(type,'').trim().split(',')
+    if(type=='dd'){
+        val=val.map(v=>{
+            if(v.indexOf('RVA')>-1){
+                v = v.replace('RVA','').trim()
+                v = LE(DectoHex4(v))
+            }else{
+                v = DectoHex4(v)
+            }
+            return v
+        })
+        return val.join(' ')
+    }
+    if(type=='dq'){
+        val=val.map(v=>{
+            if(v.indexOf('RVA')>-1){
+                v = v.replace('RVA','').trim()
+                v = LE(DectoHex8(v))
+            }else{
+                v = DectoHex8(v)
+            }
+            return v
+        })
+        return val.join(' ')
+    }
+    if(type=='dw'){
+        val=val.map(v=>{
+            return DectoHex2(v)
+        })
+        return val.join(' ')
+    }
+    if(type=='db'){
+        val=val[0].split('\'')[1]
+        let ret = ''
+        for(let i=0;i<val.length;i++){
+            let code = val.charCodeAt(i)
+            ret += DectoHex1(code)
+        }
+        return ret+'00'
+    }
+    return line
+})
+
+
+
+idata = lines.join('\n')
+
+fs.writeFileSync('./idata.txt',idata)
